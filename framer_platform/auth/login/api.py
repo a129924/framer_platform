e7ab58c4260bdb707bc323c15c3812496ee14bf6
a129924 <a129924@gmail.com>
2023-10-11 20:55:22 +0800

    ADD

--- framer_platform/auth/login/api.py
+++ framer_platform/auth/login/api.py
@@ -1,8 +1,10 @@
+from typing import Annotated
+
 from framer_platform.auth.schemas import UserLogin
 from framer_platform.database import get_async_session, AsyncSession
 from framer_platform.database.models.regester_model import User
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Cookie, Response
 from fastapi.responses import JSONResponse
 
 login_router = APIRouter(tags=["login"])
@@ -19,10 +21,25 @@ async def login(
     )
 
     if user_data:
-        return JSONResponse(
+        response = JSONResponse(
             content={"status": "OK", "statusText": "success"}, status_code=200
         )
+        response.set_cookie(key="userLogin", value=user_login.username, max_age=600)
+
+        return response
     else:
         return JSONResponse(
             content={"status": "Failed", "statusText": "no user found"}, status_code=401
         )
+
+
+@login_router.post("/userLogout", status_code=200)
+def logout(response: Response, userLogin: str = Cookie(None)) -> JSONResponse:
+    response.delete_cookie("userLogin")
+
+    return JSONResponse(content={"username": userLogin})
+
+
+@login_router.get("/get_user_login_cookie")
+def get_user_login_cookie(userLogin: Annotated[str | None, Cookie()] = None) -> dict:
+    return {"userLogin": userLogin}
